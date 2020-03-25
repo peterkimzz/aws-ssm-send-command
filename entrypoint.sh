bcf3d55cd6ee1e8430a10dae1f8a7ab13113ddba
Peter Kim <peterkimzz69@gmail.com>
2020-03-25 14:42:44 +0900

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Instance Id $1"
-echo "Run-Command $2"
-echo "Document-Name $3"
-echo "Comment $4"
-echo "Working Directory $5"
+# 이 커맨드 아래 명령어 중 에러가 발생하면 즉시 스크립트 실행을 중단시킴
+set -e
 
-time=$(date)
-instance-id=$1
-commands=$2
-document-name=$3
+function main() {
+  sanitize "${INPUT_INSTANCE_ID}" "instnace_id"
+  sanitize "${INPUT_COMMANDS}" "commands"
 
-# Run AWS CLI
-aws ssm send-command \
-  --instance-ids ${instance-id} \
-  --document-name "AWS-RunShellScript" \
-  --comment "Blue/Green Deployment" \
-  --parameters \
-    '{"workingDirectory": ["/home/ubuntu/api_production"], "commands":["/bin/sh deploy.sh"]}'
+  init
+  send_command
+}
 
-# End
-exit 0;
+function sanitize() {
+  if [ -z "${1}" ]; then
+    >&2 echo "${2} is required. Did you set parameter ${2}?"
+    exit 1
+  fi
+}
+
+function init() {
+  export INSTANCE_ID=$INPUT_INSTANCE_ID
+  export COMMANDS=$INPUT_COMMANDS
+  export DOCUMENT_NAME=$INPUT_DOCUMENT_NAME
+
+  export COMMENT=$INPUT_COMMENT
+  export WORKING_DIRECTORY=$INPUT_WORKING_DIRECTORY
+}
+
+function send_command() {
+  echo "== START SEND-COMMAND"
+  aws ssm send-command \
+    --instance-ids ${INSTANCE_ID} \
+    --document-name ${DOCUMENT_NAME} \
+    --comment ${COMMENT} \
+    --parameters \
+      "{"workingDirectory": ["${WORKING_DIRECTORY}"], "commands":["${COMMENT}"]}"
+
+  echo "== FINISHED SEND_COMMAND"
+}
+
+main
