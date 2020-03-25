afac6297052decf60bde3589e94cc0e7bf4b106e
Peter Kim <peterkimzz69@gmail.com>
2020-03-25 15:22:58 +0900

    test case

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ function send_command() {
   SEND_COMMAND_CMD=$(aws ssm send-command \
     --instance-ids ${INSTANCE_ID} \
     --document-name ${DOCUMENT_NAME} \
-    --parameters "\'{"workingDirectory": ["${WORKING_DIRECTORY}"], "commands":["${COMMANDS}"]}\'")
+    --parameters "\"{"workingDirectory": ["${WORKING_DIRECTORY}"], "commands":["${COMMANDS}"]}\"")
 
   echo $SEND_COMMAND
 
